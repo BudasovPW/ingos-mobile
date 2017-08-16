dc868472ae4c839e5e9b82b21f83b76f87a6c88f
BarnabyJ <vlaschenko.michael@gmail.com>
2017-08-16 19:11:08 +0300

    Trust managment pages.

--- site/assets/js/vendors/charts.js
+++ site/assets/js/vendors/charts.js
@@ -21,52 +21,6 @@ $(function () {
         }
     });
 
-    /*
-    $('#trust-managment-portfolio').highcharts({
-        chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: 0,
-            plotShadow: false,
-            spacingTop: -10,
-            spacingBottom: -10,
-            spacingLeft: -10,
-            spacingRight: -10
-        },
-        title: {
-            text: '<span class="chart-pie__title">Общая оценка</span><br> <span class="chart-pie__value">1 122 980,06 руб</span>',
-            align: 'center',
-            verticalAlign: 'middle',
-            y: -5
-        },
-        tooltip: false,
-        plotOptions: {
-            pie: {
-                borderWidth: 0,
-                dataLabels: {
-                    enabled: false,
-                },
-                size: '100%',
-                startAngle: -200,
-                endAngle: 160
-            }
-        },
-        series: [{
-            type: 'pie',
-            name: 'Browser share',
-            innerSize: 220,
-            data: [
-                ['Облигации', 86.78],
-                ['Прочие вложения', 12.21],
-                ['Денежные средства у брокера', 1.01],
-            ],
-            states: {
-                hover: {
-                    enabled: false
-                }
-            }
-        }]
-    });*/
-
     $('#portfolio-structure').highcharts({
         chart: {
             plotBackgroundColor: "#f9f9fa",
@@ -138,6 +92,45 @@ $(function () {
                 ['Ингосстрах. Облигации', 59.69],
                 ['Ингосстрах. Еврооблигации', 32.03],
                 ['Ингосстрах. Риэл Эстейт', 8.28]
+            ],
+            states: {
+                hover: {
+                    enabled: false
+                }
+            }
+        }]
+    });
+
+    $('#trust-managment-portfolio').highcharts({
+        chart: {
+            plotBackgroundColor: "#f9f9fa",
+            plotBorderWidth: 0,
+            plotShadow: false,
+            spacingTop: -10,
+            spacingBottom: -10,
+            spacingLeft: -10,
+            spacingRight: -10
+        },
+        title: false,
+        tooltip: false,
+        plotOptions: {
+            pie: {
+                borderWidth: 0,
+                dataLabels: {
+                    enabled: false,
+                },
+                size: '100%',
+                startAngle: 10,
+                endAngle: 370
+            }
+        },
+        series: [{
+            type: 'pie',
+            innerSize: 120,
+            data: [
+                ['Облигации', 96.78],
+                ['Прочие вложения', 2.21],
+                ['Денежные средства у брокера', 1.01]
             ],
             states: {
                 hover: {
@@ -224,291 +217,6 @@ $(function () {
 
     });
 
-    /*$('#trust-managment-portfolio-dynamics').highcharts({
-        chart: {
-            type: 'line',
-            spacingLeft: 0
-        },
-        title: false,
-        xAxis: {
-            categories: [
-                '1 <br> ЯНВ <br> 2011',
-                '1 <br> ЯНВ <br> 2012',
-                '1 <br> ЯНВ <br> 2013',
-                '1 <br> ЯНВ <br> 2014',
-                '1 <br> ЯНВ <br> 2015',
-                '1 <br> ЯНВ <br> 2016',
-                '1 <br> ЯНВ <br> 2017'
-            ],
-            tickWidth: 0,
-        },
-
-        yAxis: {
-            title: false,
-            tickWidth: 0,
-            lineWidth: 0,
-            minorGridLineWidth: 0,
-            gridLineWidth: 0,
-            alternateGridColor: null,
-            labels: false
-        },
-        tooltip: {
-            formatter: function () {
-                return 'ТЕК. ОЦЕНКА: <b>' + this.y + '</b>';
-            }
-        },
-        plotOptions: {
-            spline: {
-                lineWidth: 3,
-                marker: {
-                    enabled: false
-                }
-            },
-            series: {
-                marker: {
-                    fillColor: '#FFFFFF',
-                    lineWidth: 2,
-                    lineColor: null,
-                    symbol: 'circle'
-                }
-            }
-        },
-        series: [{
-            color: '#00adeb',
-            data: [3, 4, 3, 5, 4, 10, 8]
-        }],
-        legend: false
-
-    });
-
-    /!* industry chart *!/
-    var industryData = [
-        ["Не определено", 1471.61],
-        ["Государственные ценные бумаги", 460.87]];
-
-    $('#industry-chart').highcharts({
-        chart: {
-            type: 'column',
-            inverted: true,
-            marginLeft: 360,
-            height: 220
-        },
-        title: false,
-        xAxis: {
-            tickWidth: 0,
-            labels: {
-                align: 'left',
-                x: -360,
-                useHTML: true,
-                enabled: true,
-                formatter: function () {
-                    return '<div class="charts-column__data">'
-                        + '<div class="charts-column__data-item">' + industryData[this.value][0] + '</div>'
-                        + '<div class="charts-column__data-item _count">' + industryData[this.value][1] + '</div></div>';
-                }
-            }
-        },
-        yAxis: {
-            title: false,
-            lineWidth: 1
-
-        },
-        legend: {
-            enabled: false
-        },
-        plotOptions: {
-            series: {
-                borderWidth: 0,
-                states: {
-                    hover: {
-                        enabled: false
-                    }
-                }
-            }
-        },
-        tooltip: false,
-        series: [{
-            color: '#00adeb',
-            pointWidth: 50,
-            data: industryData
-        }]
-
-    });
-
-    /!* assets classes chart *!/
-    var assetsClassesData = [
-        ["Фонды", 5071.61]];
-
-    $('#assets-classes-chart').highcharts({
-        chart: {
-            type: 'column',
-            inverted: true,
-            marginLeft: 360,
-            height: 135
-        },
-        title: false,
-        xAxis: {
-            tickWidth: 0,
-            labels: {
-                align: 'left',
-                x: -360,
-                useHTML: true,
-                enabled: true,
-                formatter: function () {
-                    return '<div class="charts-column__data">'
-                        + '<div class="charts-column__data-item">' + assetsClassesData[this.value][0] + '</div>'
-                        + '<div class="charts-column__data-item _count">' + assetsClassesData[this.value][1] + '</div></div>';
-                }
-            }
-        },
-        yAxis: {
-            title: false,
-            lineWidth: 1
-
-        },
-        legend: {
-            enabled: false
-        },
-        plotOptions: {
-            series: {
-                borderWidth: 0,
-                states: {
-                    hover: {
-                        enabled: false
-                    }
-                }
-            }
-        },
-        tooltip: false,
-        series: [{
-            color: '#00adeb',
-            pointWidth: 50,
-            data: assetsClassesData
-        }]
-
-    });
-
-    /!* currency chart *!/
-    var currencyData = [
-        ["Российский рубль", 4952.48],
-        ["Доллар США", 0],
-        ["Евро", 0],
-        ["Фунт стерлингов", 0],
-        ["Японская Йена", 0],
-        ["Швейцарский франк", 0]];
-
-    $('#currency-chart').highcharts({
-        chart: {
-            type: 'column',
-            inverted: true,
-            marginLeft: 360,
-            height: 400
-        },
-        title: false,
-        xAxis: {
-            tickWidth: 0,
-            labels: {
-                align: 'left',
-                x: -360,
-                useHTML: true,
-                enabled: true,
-                formatter: function () {
-                    return '<div class="charts-column__data">'
-                        + '<div class="charts-column__data-item">' + currencyData[this.value][0] + '</div>'
-                        + '<div class="charts-column__data-item _count">' + currencyData[this.value][1] + '</div></div>';
-                }
-            }
-        },
-        yAxis: {
-            title: false,
-            lineWidth: 1
-
-        },
-        legend: {
-            enabled: false
-        },
-        plotOptions: {
-            series: {
-                borderWidth: 0,
-                states: {
-                    hover: {
-                        enabled: false
-                    }
-                }
-            }
-        },
-        tooltip: false,
-        series: [{
-            color: '#00adeb',
-            pointWidth: 30,
-            data: currencyData
-        }]
-
-    });
-
-    /!* issuers chart *!/
-    var issuersData = [
-        ["МинФин России", 1628.99],
-        ["ОАО «Силовые машины»", 267.1],
-        ["Правительство Москвы в лице Департамента финансов г. Москвы", 263.82],
-        ["ПАО «Магнит»", 258.95],
-        ["ОАО «Акрон»", 245.25],
-        ["ПАО «ВымпелКом»", 245.07],
-        ["ООО «ИКС 5 ФИНАНС»", 225.79],
-        ["АФК Система»", 206.85],
-        ["ООО «МегаФон Финанс»", 202.41],
-        ["ОАО «Полюс Золото»", 197.91],
-        ["Другое", 1096.54]];
-
-    $('#issuers-chart').highcharts({
-        chart: {
-            type: 'column',
-            inverted: true,
-            marginLeft: 360,
-            height: 735
-        },
-        title: false,
-        xAxis: {
-            tickWidth: 0,
-            labels: {
-                align: 'left',
-                x: -360,
-                useHTML: true,
-                enabled: true,
-                formatter: function () {
-                    return '<div class="charts-column__data">'
-                        + '<div class="charts-column__data-item">' + issuersData[this.value][0] + '</div>'
-                        + '<div class="charts-column__data-item _count">' + issuersData[this.value][1] + '</div></div>';
-                }
-            }
-        },
-        yAxis: {
-            title: false,
-            lineWidth: 1
-
-        },
-        legend: {
-            enabled: false
-        },
-        plotOptions: {
-            series: {
-                borderWidth: 0,
-                states: {
-                    hover: {
-                        enabled: false
-                    }
-                }
-            }
-        },
-        tooltip: false,
-        series: [{
-            color: '#00adeb',
-            pointWidth: 30,
-            data: issuersData
-        }]
-
-    });
-
     if(Highcharts.stockChart){
         $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function (data) {
 
@@ -516,16 +224,25 @@ $(function () {
             var chart = Highcharts.stockChart('history-chart', {
 
                 chart: {
-                    height: 400
+                    height: 350,
+                    spacingLeft: -10,
+                    spacingRight: -10
                 },
                 title: false,
                 rangeSelector: {
                     enabled: false
                 },
                 xAxis: {
-                    tickWidth: 0
+                    tickWidth: 0,
+                    lineWidth: 0,
+                    minorGridLineWidth: 0,
+                    gridLineWidth: 0,
+                    labels: {
+                        enabled: false
+                    }
                 },
                 yAxis: {
+                    tickWidth: 0,
                     gridLineWidth: 0,
                     labels: {
                         enabled: false
@@ -540,7 +257,7 @@ $(function () {
                             symbol: 'circle',
                             states: {
                                 hover: {
-                                    lineWidth: 3,
+                                    lineWidth: 2,
                                     radius: 6
                                 }
                             }
@@ -550,45 +267,27 @@ $(function () {
                         lineWidth: 2,
                         states: {
                             hover: {
-                                lineWidth: 3
+                                lineWidth: 2
                             }
                         }
                     }
                 },
-                scrollbar: {
-                    barBackgroundColor: '#9ea3a9',
-                    barBorderWidth: 0,
-                    buttonBackgroundColor: '#9ea3a9',
-                    buttonBorderWidth: 0,
-                    buttonArrowColor: '#e1e1e1',
-                    rifleColor: '#e1e1e1',
-                    trackBackgroundColor: '#f1f2f2',
-                    trackBorderWidth: 1,
-                    trackBorderColor: '#dcdedf'
-                },
-                navigator: {
-                    series: {
-                        color: '#f9f9fa',
-                        lineColor: '#dcdedf',
-                        lineWidth: 1
-                    },
-                    maskFill: 'rgba(204, 239, 251, 0.75)'
-                },
+                scrollbar: false,
+                navigator: false,
                 tooltip: {
-                    useHTML: true,
-                    crosshairs: false,
+                    crosshairs: {
+                        zIndex: 2
+                    },
+                    shared: true,
                     formatter: function () {
-                        return '<div class="tooltip-container">'
-                            + '<div class="tooltip-col"><span class="tooltip-title">ДАТА:</span>'
-                            + '<span class="tooltip-value">' + Highcharts.dateFormat('%e %b %Y', new Date(this.x)) + '</span></div>'
-                            + '<div class="tooltip-col"><span class="tooltip-title">ИЗМЕНЕНИЕ:</span>'
-                            + '<span class="tooltip-value _green">' + Highcharts.numberFormat(this.y, 2) + ' % </span></div></div></div>';
+                        return false;
                     }
                 },
                 series: [{
                     data: data,
                     type: 'area',
                     color: '#cceffb',
+                    fillOpacity: 0.75,
                     threshold: null,
                 }]
 
@@ -596,103 +295,5 @@ $(function () {
         });
 
     }
-
-    if(Highcharts.data){
-        // Load the data from a Google Spreadsheet
-        // https://docs.google.com/a/highsoft.com/spreadsheet/pub?hl=en_GB&hl=en_GB&key=0AoIaUO7wH1HwdFJHaFI4eUJDYlVna3k5TlpuXzZubHc&output=html
-        Highcharts.data({
-
-            googleSpreadsheetKey: '0AoIaUO7wH1HwdFJHaFI4eUJDYlVna3k5TlpuXzZubHc',
-
-            // custom handler when the spreadsheet is parsed
-            parsed: function (columns) {
-
-                // Read the columns into the data array
-                var data = [];
-                $.each(columns[0], function (i, code) {
-                    data.push({
-                        code: code.toUpperCase(),
-                        value: parseFloat(columns[2][i]),
-                        name: columns[1][i]
-                    });
-                });
-
-                // Initiate the chart
-                $('#geography-chart').highcharts('Map', {
-                    chart: {
-                        spacingLeft: -10,
-                        spacingRight: -10,
-                        height: 450
-                    },
-                    title: false,
-                    mapNavigation: {
-                        enabled: false
-                    },
-                    legend: {
-                        align: 'left',
-                        verticalAlign: 'bottom',
-                        floating: true,
-                        layout: 'vertical',
-                        valueDecimals: 0,
-                        itemMarginTop: 18,
-                        symbolRadius: 0,
-                        symbolWidth: 35,
-                        symbolHeight: 30,
-                        useHTML: true,
-                        y: 20
-                    },
-
-                    colorAxis: {
-                        dataClasses: [{
-                            from: 0,
-                            to: 3,
-                            color: '#b9eafc'
-                        }, {
-                            from: 3,
-                            to: 10,
-                            color: '#acdff6'
-                        }, {
-                            from: 10,
-                            to: 30,
-                            color: '#74cdf6'
-                        }, {
-                            from: 30,
-                            to: 100,
-                            color: '#44c4f9'
-                        }, {
-                            from: 100,
-                            to: 300,
-                            color: '#30b8f6'
-                        }, {
-                            from: 300,
-                            to: 1500,
-                            color: '#00adeb'
-                        }]
-                    },
-
-                    series: [{
-                        data: data,
-                        mapData: Highcharts.maps['custom/world'],
-                        joinBy: ['iso-a2', 'code'],
-                        animation: true,
-                        name: 'ЗАНЧЕНИЕ',
-                        color: '#b9eafc',
-                        borderColor: '#ffffff',
-                        states: {
-                            hover: {
-                                color: '#00adeb'
-                            }
-                        }
-                    }]
-                });
-            },
-            error: function () {
-                $('#geography-chart').html('<div class="loading">' +
-                    '<i class="icon-frown icon-large"></i> ' +
-                    'Проблемы с загрузкой данных' +
-                    '</div>');
-            }
-        });
-    }*/
 
 })
